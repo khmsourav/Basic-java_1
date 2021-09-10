87a3f5c6981b4c97e6d7fa6ab3489165e95d776d
Kazi Sourav <32020708+khmsourav@users.noreply.github.com>
2021-09-10 23:44:13 +0600

    Add files via upload

---
+++ basic_01.java
@@ -0,0 +1,21 @@
+public class basic_01{
+public static void main(String args[]){
+	//int a = 002431;
+	//System.out.println("My name is Kazi Sourav");
+	//System.out.println("My phone number is " +a);
+	
+	int a = 5;
+	int b = 10;
+	
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a*b);
+	System.out.println(a/b);
+	System.out.println(a%b);
+	/*int a = 3;
+	//int b = 2147483647;
+	//System.out.println(a+b);*/
+	
+	
+}
+}
